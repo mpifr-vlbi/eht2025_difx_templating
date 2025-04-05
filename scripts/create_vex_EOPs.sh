d7c78462a5ebb0a69796b19cc3e20e8550ccda80
oper <oper@mpifr.de>
2025-04-05 02:03:26 +0200

    fix schedule name

--- scripts/create_vex_EOPs.sh
+++ scripts/create_vex_EOPs.sh
@@ -5,7 +5,7 @@ OUTDIR=../templates/common_sections/
 
 pushd $SCRIPT_DIR  2>&1 > /dev/null
 
-./geteop.pl 2025-092  5 $OUTDIR/eop_e25c05.vex
+./geteop.pl 2025-092  5 $OUTDIR/eop_e25c04.vex
 
 popd  2>&1 > /dev/null
 

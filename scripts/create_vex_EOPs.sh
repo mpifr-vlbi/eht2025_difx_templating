5f5ce4cd24d6c6c26a1757dfce1c067a22bbb345
oper <oper@mpifr.de>
2025-09-23 17:10:58 +0200

    add e25e09 initial EOPs

--- scripts/create_vex_EOPs.sh
+++ scripts/create_vex_EOPs.sh
@@ -8,6 +8,7 @@ pushd $SCRIPT_DIR  2>&1 > /dev/null
 ./geteop.pl 2025-092  5 $OUTDIR/eop_e25c04.vex
 ./geteop.pl 2025-094  5 $OUTDIR/eop_e25a06.vex
 ./geteop.pl 2025-096  5 $OUTDIR/eop_e25f08.vex
+./geteop.pl 2025-097  5 $OUTDIR/eop_e25e09.vex
 
 popd  2>&1 > /dev/null
 
